8a19c1f63e3624f49b281e7d14ca015857f397e2
Labs (devops-success-true) <kastriot.ymeri.labs@gmail.com>
2025-09-27 21:58:08 +0200

    Initial commit EKS cluster setup

---
+++ 02backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "expensee-dev"
+    key    = "expense-dev"
+    region = "us-east-1"
+    # use_lockfile      = true # enables native locking
+    dynamodb_endpoint = "expensee-locking" #The parameter "dynamodb_endpoint" is deprecated. Use parameter "endpoints.dynamodb" instead.
+  }
+}
+    
