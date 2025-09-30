370b069982a32c721699b5ae6d9d5222255aee38
Labs <117577809+devops-success-true@users.noreply.github.com>
2025-09-30 15:35:38 +0200

    Update rbac.tf

--- rbac.tf
+++ rbac.tf
@@ -16,3 +16,20 @@ resource "kubernetes_cluster_role_binding" "devops_admin" {
     api_group = "rbac.authorization.k8s.io"
   }
 }
+
+
+#  The DevOps IAM role (outside this repo, in your IAM account) must have permissions like:
+# {
+#   "Version": "2012-10-17",               # IAM policy language version (always use 2012-10-17)
+#   "Statement": [                         # Array of permission statements
+#     {
+#       "Effect": "Allow",                 # This statement grants (Allow) the permissions
+#       "Action": [                        # Actions this policy will permit
+#         "eks:DescribeCluster",           # Lets IAM principal describe EKS cluster details (endpoint, cert, etc.)
+#         "eks:AccessKubernetesApi"        # Grants permission to call the Kubernetes API via IAM Auth (required in API mode)
+#       ],
+#       "Resource": "*"                    # Scope of resources: here it’s all EKS clusters in the account
+#     }
+#   ]
+# }
+
