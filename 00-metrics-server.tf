cf0b6f18d2dd4def1469db95ee0687e7bce7cd4e
Labs <117577809+devops-success-true@users.noreply.github.com>
2025-10-02 21:45:54 +0200

    Create 00-metrics-server.tf

---
+++ 00-metrics-server.tf
@@ -0,0 +1,12 @@
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  namespace  = "kube-system"
+  version    = "3.12.1"
+
+  values = [file("${path.module}/values/metrics-server.yaml")]
+
+  # Make sure EKS cluster is ready before Helm tries to connect
+  depends_on = [module.eks]
+}
